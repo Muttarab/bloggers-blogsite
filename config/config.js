4a78aa2ca2d0572d72df14a32aec0a5369fff47d
Muttarab Ahmad <muttarabahmad9@gmail.com>
2022-04-01 17:08:06 +0500

    ready for deployment on heroku

--- config/config.js
+++ config/config.js
@@ -6,21 +6,21 @@ module.exports={
 		"password": process.env.PG_PASSWORD,
 		"database": process.env.PG_DATABASE,
 		"host": process.env.PG_HOST,
-		"dialect": process.env.PG_DIALECT
+		"dialect": postgres
 	},
 	"test": {
 		"username": process.env.PG_USER,
 		"password": process.env.PG_PASSWORD,
 		"database": process.env.PG_DATABASE,
 		"host": process.env.PG_HOST,
-		"dialect": process.env.PG_DIALECT
+		"dialect": postgres
 	},
 	"production": {
 		"username": process.env.PG_USER,
 		"password": process.env.PG_PASSWORD,
 		"database": process.env.PG_DATABASE,
 		"host": process.env.PG_HOST,
-		"dialect": process.env.PG_DIALECT,
+		"dialect": postgres,
 		"use_env_variable": "DATABASE_URL",
 		dialectOptions: {
 		  ssl: {
