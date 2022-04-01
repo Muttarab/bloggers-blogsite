511ee6d3380a89b9bfcb33e8809f5ffa7a164415
Muttarab Ahmad <muttarabahmad9@gmail.com>
2022-04-01 17:36:17 +0500

    ready for deployment on heroku

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
-const path=require('path')
-require('dotenv').config();
+
 const env = process.env.NODE_ENV || "development";
-const config = require(path.join(__dirname, '../config/config.js'))
+const config = require(`../config/config.js`)[env];
+
 const db = {};
 let sequelize;
 if (config.use_env_variable) {
@@ -15,4 +15,10 @@ if (config.use_env_variable) {
     config
   );
 }
+// db.Post = require("./post")(sequelize, Sequelize.DataTypes);
+// console.log(db, "@db");
+// db.Post.associate(db);
+
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
 module.exports = sequelize;
