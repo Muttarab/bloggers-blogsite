50a954eb710ec7810698475c2057d8724731fe39
Muttarab Ahmad <muttarabahmad9@gmail.com>
2022-04-01 21:35:42 +0500

    ready for deployment on heroku

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ dotenv.config();
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 const cors = require('cors');
-const PORT = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
+const host = '0.0.0.0'
 const path = require('path')
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false, limit: '5mb' }));
@@ -16,7 +17,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+app.listen(port, host, ()=> connsole.log(`server is running on port ${port}`))
 
-const server = app.listen(PORT, "localhost", function () {
-  console.log(`Blog App listening at http://localhost:${PORT}`);
-});
+
