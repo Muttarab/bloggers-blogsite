562725604876429cdb98cda4be923edb6cec98d7
Muttarab Ahmad <muttarabahmad9@gmail.com>
2022-04-01 17:19:29 +0500

    ready for deployment on heroku

--- config/config.js
+++ config/config.js
@@ -2,26 +2,26 @@ const dotenv = require("dotenv")
 dotenv.config();
 module.exports={
 	"development": {
-		"username": process.env.PG_USER,
-		"password": process.env.PG_PASSWORD,
-		"database": process.env.PG_DATABASE,
-		"host": process.env.PG_HOST,
-		"dialect": 'postgres'
+		username: process.env.PG_USER,
+		password: process.env.PG_PASSWORD,
+		database: process.env.PG_DATABASE,
+		host: process.env.PG_HOST,
+		dialect: 'postgres'
 	},
 	"test": {
-		"username": process.env.PG_USER,
-		"password": process.env.PG_PASSWORD,
-		"database": process.env.PG_DATABASE,
-		"host": process.env.PG_HOST,
-		"dialect": 'postgres'
+		username: process.env.PG_USER,
+		password: process.env.PG_PASSWORD,
+		database: process.env.PG_DATABASE,
+		host: process.env.PG_HOST,
+		dialect: 'postgres'
 	},
 	"production": {
-		"username": process.env.PG_USER,
-		"password": process.env.PG_PASSWORD,
-		"database": process.env.PG_DATABASE,
-		"host": process.env.PG_HOST,
-		"dialect": 'postgres',
-		"use_env_variable": "DATABASE_URL",
+		username: process.env.PG_USER,
+		password: process.env.PG_PASSWORD,
+		database: process.env.PG_DATABASE,
+		host: process.env.PG_HOST,
+		dialect: 'postgres',
+		use_env_variable: "DATABASE_URL",
 		dialectOptions: {
 		  ssl: {
 			require: true,
