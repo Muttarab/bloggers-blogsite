f40aadab8395126096cc8230a37c9f8099f4b14c
Muttarab Ahmad <muttarabahmad9@gmail.com>
2022-04-01 21:51:23 +0500

    ready for deployment on heroku

--- app.js
+++ app.js
@@ -5,18 +5,17 @@ dotenv.config();
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 const cors = require('cors');
-const port = process.env.PORT || 8000;
-const host = '0.0.0.0'
+const PORT = process.env.PORT || 8000;
 const path = require('path')
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false, limit: '5mb' }));
 app.use(bodyParser.json());
+app.use(express.static('build')); 
 app.use("/", routes);
 app.use(express.static('./images'));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port, host, ()=> connsole.log(`server is running on port ${port}`))
+app.listen(PORT, () => { // start server and listen on specified port
+  console.log(`App is running on ${PORT}`) // confirm server is running and log port to the console
+}) 
 
 
